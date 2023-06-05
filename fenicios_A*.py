34fcca517113c63da7064162543c54979f1c1add
RamiroNB <ramiro.nilson.barros@gmail.com>
2023-06-05 11:37:18 -0300

    A*?

--- fenicios_A*.py
+++ fenicios_A*.py
@@ -1,14 +1,71 @@
+import math
 import time
+from queue import PriorityQueue
+
+dr = [-1, +1, 0, 0]  # up down right left
+dc = [0, 0, +1, -1]  # up down right left
 
 
-def find_voyage_cost(f_map, ports, start, end):
-    pass
+def find_voyage_cost_a_star(f_map, ports, start, end):
+    start = ports[start]
+    end = ports[end]
+    count = 0
+    pq = PriorityQueue()
+    pq.put((0, count, tuple(start)))
+    came_from = {}
+    g_score = {}
+    f_score = {}
+    for i, row in enumerate(f_map):
+        for j, column in enumerate(row):
+            g_score[tuple([i, j])] = float("inf")
+            f_score[tuple([i, j])] = float("inf")
+    g_score[tuple(start)] = 0
+    f_score[tuple(start)] = manhattan_distance(start, end)  # heuristic
+    open_set_hash = {tuple(start)}
+
+    while not pq.empty():
+        current = pq.get()[2]
+        open_set_hash.remove(current)
+
+        if current == tuple(end):
+            return g_score[current]
+
+        for neighbor in get_neighbors(current[0], current[1], f_map):
+            temp_g_score = g_score[current] + 1
+
+            if temp_g_score < g_score[tuple(neighbor)]:
+                came_from[tuple(neighbor)] = current
+                g_score[tuple(neighbor)] = temp_g_score
+                f_score[tuple(neighbor)] = temp_g_score + manhattan_distance(
+                    neighbor, end
+                )
+                if tuple(neighbor) not in open_set_hash:
+                    count += 1
+                    pq.put((f_score[tuple(neighbor)], count, tuple(neighbor)))
+                    open_set_hash.add(tuple(neighbor))
+
+    return -1
+
+
+def get_neighbors(row, column, f_map):
+    neighbors = []
+    r, c = row, column  # row, column
+    for i in range(4):
+        rr = r + dr[i]
+        cc = c + dc[i]
+        if 0 <= rr < len(f_map) and 0 <= cc < len(f_map[0]) and f_map[rr][cc] != "*":
+            neighbors.append([rr, cc])
+    return neighbors
+
+
+def manhattan_distance(start, end):
+    return abs(start[0] - end[0]) + abs(start[1] - end[1])
 
 
 def main():
     f_map = []
     ports = dict()
-    with open("casos/caso01.txt", "r") as f:
+    with open("casos/caso20.txt", "r") as f:
         lines = f.readlines()
         print(lines[0])
         lines.remove(lines[0])
@@ -37,20 +94,33 @@ def main():
                     ports["9"] = [i, j]
             f_map.append(n_line)
 
-    # print(len(f_map))
-    # print(len(f_map[0]))
-    # print(len(ports))
-    # print(ports)
-    # print(f_map[ports["1"][0]][ports["1"][1]])
-    # print(f_map[ports["2"][0]][ports["2"][1]])
-    # print(f_map[ports["3"][0]][ports["3"][1]])
-    # print(f_map[ports["4"][0]][ports["4"][1]])
-    # print(f_map[ports["5"][0]][ports["5"][1]])
-    # print(f_map[ports["6"][0]][ports["6"][1]])
-    # print(f_map[ports["7"][0]][ports["7"][1]])
-    # print(f_map[ports["8"][0]][ports["8"][1]])
-    # print(f_map[ports["9"][0]][ports["9"][1]])
-    # print(f_map)
+    start = time.time()
+
+    total = 0
+    currrent = 0
+    for i in range(8):
+        if currrent == -1:
+            print("no hay ruta")
+            currrent = find_voyage_cost_a_star(
+                f_map,
+                ports,
+                str(i),
+                str(i + 2),
+            )
+        else:
+            currrent = find_voyage_cost_a_star(
+                f_map,
+                ports,
+                str(i + 1),
+                str(i + 2),
+            )
+
+        if currrent != -1:
+            last_valid = i + 2
+            total = total + currrent
+    total = total + find_voyage_cost_a_star(f_map, ports, str(last_valid), str(1))
+
+    print(total)
 
 
 if __name__ == "__main__":
